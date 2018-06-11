4878e2abc174a270562234ba1c6a9792cb76f732
koleber <koleber@estaxi.ru>
2018-06-11 17:44:31 +0600

    Fix error line

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-package com.parse.starter;.idea gitignore
+package com.parse.starter;
 
 import android.content.Intent;
 import android.os.Bundle;
